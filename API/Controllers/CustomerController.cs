48d4f76c02bc66afeeb4673351b1d55a2195a2d9
Alex Breskin <alexdbreskin@gmail.com>
2022-01-22 13:59:57 +1100

    ✨ Implemented NSwag documentation for controllers

--- API/Controllers/CustomerController.cs
+++ API/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 
 namespace API.Controllers
@@ -7,6 +8,15 @@ namespace API.Controllers
     [Route("[controller]")]
     public class CustomerController : ControllerBase
     {
+        /// <summary>
+        /// Adds a new customer to the database
+        /// </summary>
+        /// <param name="firstname">Customer's first name</param>
+        /// <param name="lastname">Customer's last name</param>
+        /// <param name="dateOfBirth">Customer's date of birth</param>
+        /// <returns>Whether the action was completed - either 201 or 400</returns>
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [HttpPut]
         public IActionResult AddCustomer(string firstname, string lastname, DateTime dateOfBirth)
         {
